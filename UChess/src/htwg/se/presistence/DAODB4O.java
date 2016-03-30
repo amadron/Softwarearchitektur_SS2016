dc4720afb9a49d4fba43ea8074e8332726aba5f4
Benedict Roth <beroth@htwg-konstanz.de>
2016-03-30 15:34:15 +0200

    Started to implement DAODB4O

--- UChess/src/htwg/se/presistence/DAODB4O.java
+++ UChess/src/htwg/se/presistence/DAODB4O.java
@@ -3,28 +3,54 @@ package htwg.se.presistence;
 /**
  * Created by benedict on 23.03.16.
  */
-import htwg.se.presistence.IDataAccessObject;
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
 
 public class DAODB4O implements IDataAccessObject {
+    String dbName = "db4oChess";
+    ObjectContainer db;
+
+    private ObjectContainer openDb(){
+        ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), dbName);
+    }
 
     @Override
     public void create(Object object) {
-
+        db = openDb();
+        try {
+            db.store(object);
+        } finally {
+            db.close();
+        }
     }
 
     @Override
     public Object read() {
+        db = openDb();
+        try {
 
-        return null;
+        } finally {
+            db.close();
+        }
     }
 
     @Override
     public void update(Object object) {
+        db = openDb();
+        try{
 
+        } finally {
+            db.close();
+        }
     }
 
     @Override
     public void delete(Object object) {
+        db = openDb();
+        try{
 
+        } finally {
+            db.close();
+        }
     }
 }
