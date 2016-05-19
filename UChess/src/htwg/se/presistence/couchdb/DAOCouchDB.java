9a60d471c7a694b16e99878f0e584a048f033a2e
Marco Boxler <Marco.boxler@htwg-konstanz.de>
2016-05-19 15:32:50 +0200

    delete DAODB4O

--- UChess/src/htwg/se/presistence/couchdb/DAOCouchDB.java
+++ UChess/src/htwg/se/presistence/couchdb/DAOCouchDB.java
@@ -1,24 +1,26 @@
-package htwg.se.presistence;
+package htwg.se.presistence.couchdb;
+
+import htwg.se.presistence.IDataAccessObject;
 
 /**
  * Created by benedict on 06.04.16.
  */
 public class DAOCouchDB implements IDataAccessObject{
 
-    private CouchDbConnector db = null;
-
-    public DAOCouchDB() {
-        HttpClient client = null;
-        try {
-            client = new StdHttpClient.Builder().url(
-                    "http://lenny2.in.htwg-konstanz.de:5984").build();
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        CouchDbInstance dbInstance = new StdCouchDbInstance(client);
-        db = dbInstance.createConnector("sudoku_db", true);
-    }
+//    private CouchDbConnector db = null;
+//
+//    public DAOCouchDB() {
+//        HttpClient client = null;
+//        try {
+//            client = new StdHttpClient.Builder().url(
+//                    "http://lenny2.in.htwg-konstanz.de:5984").build();
+//
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+//        CouchDbInstance dbInstance = new StdCouchDbInstance(client);
+//        db = dbInstance.createConnector("sudoku_db", true);
+//    }
 
     @Override
     public void create(Object object) {
