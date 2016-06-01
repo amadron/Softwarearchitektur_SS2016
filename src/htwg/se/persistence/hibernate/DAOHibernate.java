8841a480e104c172978eef2cb88c6ee7ddde55f6
Benedict Roth <beroth@htwg-konstanz.de>
2016-06-01 14:38:32 +0200

    Update

---
+++ src/htwg/se/persistence/hibernate/DAOHibernate.java
@@ -0,0 +1,28 @@
+package htwg.se.persistence.hibernate;
+
+import htwg.se.persistence.IDataAccessObject;
+
+/**
+ * Created by benedict on 25.05.16.
+ */
+public class DAOHibernate implements IDataAccessObject {
+    @Override
+    public void create(Object object) {
+
+    }
+
+    @Override
+    public Object read() {
+        return null;
+    }
+
+    @Override
+    public void update(Object object) {
+
+    }
+
+    @Override
+    public void delete(Object object) {
+
+    }
+}
